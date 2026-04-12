8649de61e95afa58d6f80fc124262392576885b1
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2026-04-13 01:54:16 +0200

    tags updated

--- src/test-data/test-tags.ts
+++ src/test-data/test-tags.ts
@@ -1,14 +1,10 @@
 // tags.ts
 export const tags = {
-  esg: '@ESG',
   e2e: '@E2E',
   api: '@API',
-  regression: '@REGRESSION',
-  bug: '@BUG',
-  locale: '@LOCALE',
 } as const;
 
-export type TagKey = keyof typeof tags; // 'e2e' | 'api' | 'regression' | ... | 'esg'
-export type Tag = (typeof tags)[TagKey]; // '@E2E' | '@API' | '@REGRESSION' | ... | '@ESG'
+export type TagKey = keyof typeof tags; // 'e2e' | 'api'
+export type Tag = (typeof tags)[TagKey]; // '@E2E' | '@API'
 
 export const pickTags = (...keys: TagKey[]): Tag[] => keys.map((k) => tags[k]);
